07998b28b5d9c9a08611a581589968de96fa2d63
savoptik <sav70792@gmail.com>
2019-07-30 01:19:34 +0300

    Initial Commit

---
+++ version_verificater/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  version_verificater
+//
+//  Created by Артём Семёнов on 30.07.2019.
+//  Copyright © 2019 Артём Семёнов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
